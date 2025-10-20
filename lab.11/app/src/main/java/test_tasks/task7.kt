6ecc7812be9f6b775d515832256aa947298d68b0
vladi <vladi@LAPTOP-GVRNVL11>
2025-10-20 13:47:27 +0300

    Introduction to Kotlin ends

---
+++ lab.11/app/src/main/java/test_tasks/task7.kt
@@ -0,0 +1,62 @@
+package test_tasks
+
+//fun main() {
+//    val count: Int = 10
+//    println("You have $count unread messages.")
+//}
+
+//fun main() {
+//    val numberOfPhotos = 100
+//    val photosDeleted = 10
+//    println("$numberOfPhotos photos")
+//    println("$photosDeleted photos deleted")
+//    println("${numberOfPhotos - photosDeleted} photos left")
+//}
+
+//fun main() {
+//    var cartTotal = 0
+//    cartTotal = 20
+//    println("Total: $cartTotal")
+//}
+
+//fun main() {
+//    var count = 10
+//    println("You have $count unread messages.")
+//    count = count + 1
+//    println("You have $count unread messages.")
+//    count++
+//    println("You have $count unread messages.")
+//}
+
+//fun main() {
+//    val nextMeeting = "Next meeting: "
+//    val date = "January 1"
+//    val reminder = nextMeeting + date
+//    println(reminder)
+//}
+
+//fun main() {
+//    val nextMeeting = "Next meeting: "
+//    val date = "January 1"
+//    val reminder = nextMeeting + date + " at work"
+//    println(reminder)
+//}
+
+//fun main() {
+//    val notificationsEnabled: Boolean = false
+//    println("Are notifications enabled? " + notificationsEnabled)
+//}
+
+/*
+ * This program displays the number of messages
+ * in the user's inbox.
+ */
+fun main() {
+    // Create a variable for the number of unread messages.
+    var count = 10
+    println("You have $count unread messages.")
+
+    // Decrease the number of messages by 1.
+    count--
+    println("You have $count unread messages.")
+}
